b19df1bc7888a96478e97af5729c1bef4da67dca
Alvaro Teran <alvaro.teran97@gmail.com>
2022-04-23 12:06:01 +0200

    Final steps in deploy

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,31 +3,7 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
-
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-//   drive() {
-//     return "broom";
-//   }
-// }
-
-// let car;
-
-// beforeEach(() => {
-//   car = new Car();
-// });
-
-// describe("Car can", () => {
-//   it("it can park", () => {
-//     assert.equal(car.park(), "stopped");
-//   });
-//   it("it can drive", () => {
-//     assert.equal(car.drive(), "broom");
-//   });
-// });
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
@@ -36,8 +12,8 @@ const inicialMessage = "Hi bro!";
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [inicialMessage] })
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: [inicialMessage] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
